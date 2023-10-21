5a9dc662395a14d1eba77771e8d17892d9ef4cb6
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-21 11:54:44 -0300

    feat: form create product

--- public/javascripts/submitProduct.js
+++ public/javascripts/submitProduct.js
@@ -1,22 +1,30 @@
 const submitProduct = (data) => {
   const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  }
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
 
-  fetch('http://localhost:3000/api/products', options)
+  fetch("http://localhost:3000/api/products", options)
     // .then(() => alert('Produto cadastrado'))
-    .catch(err => console.error(err));
-}
+    .catch((err) => console.error(err));
+};
 
+const form = document.querySelector("#form");
 
-const form = document.querySelector('#form')
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  submitProduct({
-    name: 'Mouse',
-    price: 12,
-  })
-})
+  const nameProduct = document.querySelector("#name").value;
+  const priceProduct = ++document.querySelector("#price").value;
+
+  if (nameProduct === "" || priceProduct === "") {
+    alert("Preencha todos os campos");
+  } else {
+    console.log(nameProduct, priceProduct, "teste");
+    // submitProduct({
+    //   name: nameProduct,
+    //   price: priceProduct,
+    // });
+  }
+});
