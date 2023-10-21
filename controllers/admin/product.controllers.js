abfabcead07a88e6c59da4feb2e0cde379c1a0ac
Edilson Nogueira <edilsonnsn@hotmail.com>
2023-10-21 11:28:14 -0300

    feat: Efetuado as mudanças/correções do PR e adicionando estilo na hora da seleção da pagina.

--- controllers/admin/product.controllers.js
+++ controllers/admin/product.controllers.js
@@ -1,6 +1,7 @@
 const { Product } = require('../../models/Product')
 
 const list = async (request, response) => {
+
   const { page = 1 } = request.query
 
   const limit = 10
@@ -9,17 +10,17 @@ const list = async (request, response) => {
 
   const countProduct = await Product.count()
 
-  if(countProduct !== 0){
+  if (countProduct !== 0) {
     lastpage = Math.ceil(countProduct / limit)
     console.log(lastpage)
 
-  }else{
-    response.status(400).json({mensagem: "Erro: Nenhum produto encontrado!"})
+  } else {
+    response.status(400).json({ mensagem: "Erro: Nenhum produto encontrado!" })
   }
 
-  const products = await Product.findAll({order:[['name', 'ASC']], offset: Number((page * limit) - limit), limit: limit })
+  const products = await Product.findAll({ order: [['name', 'ASC']], offset: Number((page * limit) - limit), limit: limit })
 
-  response.render('products/list', { products, lastpage })
+  response.render('products/list', { products, lastpage, page })
 }
 
 const form = (request, response) => {
