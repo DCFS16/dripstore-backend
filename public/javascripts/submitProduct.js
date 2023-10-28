eb9b35a418c6726f81d694ba3dbddafbe9220745
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-28 12:01:59 -0300

    feat: form create product

--- public/javascripts/submitProduct.js
+++ public/javascripts/submitProduct.js
@@ -1,3 +1,5 @@
+const REGEX_NUMBER = /^\d+(?:\.\d+)?$/;
+
 const submitProduct = (data) => {
   const options = {
     method: "POST",
@@ -11,17 +13,33 @@ const submitProduct = (data) => {
 };
 
 const form = document.querySelector("#form");
+const inputError = document.querySelector("#error-price");
+
+const validatePriceInput = (input) => {
+  if (REGEX_NUMBER.test(input)) {
+    inputError.classList.toggle("invisible");
+    return false;
+  } else {
+    inputError.classList.toggle("invisible");
+    return true;
+  }
+};
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nameProduct = document.querySelector("#name").value;
-  const priceProduct = ++document.querySelector("#price").value;
+  const priceProduct = document.querySelector("#price").value;
 
   if (nameProduct === "" || priceProduct === "") {
     alert("Preencha todos os campos");
+
+  } else if (validatePriceInput(priceProduct)) {
+    console.log('osdfasghldifj');
+
   } else {
-    console.log(nameProduct, priceProduct, "teste");
+    console.log(nameProduct, parseFloat(priceProduct), "teste2");
     // submitProduct({
     //   name: nameProduct,
     //   price: priceProduct,
