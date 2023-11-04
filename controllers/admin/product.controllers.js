defced13ebff4a225f52c6f76d08adad594cea55
Carlos José <carlosdigital16@gmail.com>
2023-11-04 09:09:05 -0300

    fix: solve parital conflicts

--- controllers/admin/product.controllers.js
+++ controllers/admin/product.controllers.js
@@ -11,17 +11,15 @@ const list = async (request, response) => {
 
   const countProduct = await Product.count()
 
-  if (countProduct !== 0) {
-    lastpage = Math.ceil(countProduct / limit)
-    console.log(lastpage)
-
-  } else {
-    response.status(400).json({ mensagem: "Erro: Nenhum produto encontrado!" })
-  }
+  // if (countProduct === 0) {
+  //   response.status(400).json({ mensagem: "Erro: Nenhum produto encontrado!" })
+  // } else {
+  // }
+  lastpage = Math.ceil(countProduct / limit)
 
   const products = await Product.findAll({ order: [['name', 'ASC']], offset: Number((page * limit) - limit), limit: limit })
 
-  response.render('products/list', { products, lastpage, page })
+  response.render('products/list', { products, lastpage, page, search: '' })
 }
 
 const form = (request, response) => {
