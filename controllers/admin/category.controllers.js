577808388f3d22d0a9951a19cfe809f54f9130fc
fabiola <fabiolapinheiropimentel@hotmail.com>
2023-11-18 09:17:20 -0300

    ajustando eslint5

--- controllers/admin/category.controllers.js
+++ controllers/admin/category.controllers.js
@@ -1,5 +1,5 @@
 const { Category } = require('../../models/Category')
-/* eslint-disable */
+
 const list = async (request, response) => {
   const { page = 1 } = request.query
 
@@ -8,22 +8,24 @@ const list = async (request, response) => {
   let lastpage = 1
 
   const countCategory = await Category.count()
-
+  /* eslint-disable */
   if (countCategory !== 0) {
     lastpage = Math.ceil(countCategory / limit)
   } else {
     response.status(400).json({ mensagem: 'Erro: Nenhuma categoria encontrada!' })
   }
-
+  /* eslint-enable */
   const categories = await Category.findAll({ order: [['name', 'ASC']], offset: Number((page * limit) - limit), limit })
 
   response.render('categories/list', { categories, lastpage, page })
 }
 
+/* eslint-disable */
 const form = (response) => {
   response.render('categories/form')
 }
 /* eslint-enable */
+
 module.exports = {
   list,
   form,
